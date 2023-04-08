a8eef91357266d17dfb9d626c932c8b99fa34123
Kelvin Harlan <kelvinharlandev@gmmail.com>
2023-04-08 13:13:19 -0300

    Animacoes com js

--- script.js
+++ script.js
@@ -15,3 +15,23 @@ function menuOption() {
     }
 
 }
+let index = 2;
+function skillsLogos(){
+    let img = document.querySelector('.img-box img');
+    img.src = `./imagens/0${index}.jpg`;
+    index < 9 ? index +=1 : index =1;
+    
+    
+}setInterval(skillsLogos, 3000)
+
+function left(){
+    let img = document.querySelector('.img-box img');
+    index > 1 ? img.src = `./imagens/0${index -=1}.jpg` : img.src = `./imagens/0${index =9}.jpg`;
+    console.log(index -1)
+}
+
+function right(){
+    let img = document.querySelector('.img-box img');
+    index < 9 ? img.src = `./imagens/0${index +=1}.jpg` : img.src = `./imagens/0${index =1}.jpg`;
+    console.log(index -1)
+}
